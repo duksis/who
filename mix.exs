cdc4f854b179333231f3563f037f21fb405b62c3
Hugo Duksis <hugo.duksis@gmail.com>
2013-11-07 12:37:30 +0100

    Adding meck and spec for excluding current user

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Who.Mixfile do
       compilers: [:elixir, :dynamo, :app],
       env: [prod: [compile_path: "ebin"]],
       compile_path: "tmp/#{Mix.env}/who/ebin",
-      deps: deps ]
+      deps: deps(Mix.env) ]
   end
 
   # Configuration for the OTP application
@@ -17,11 +17,20 @@ defmodule Who.Mixfile do
       mod: { Who, [] } ]
   end
 
-  defp deps do
+  defp deps(:prod) do
     [
       { :cowboy, github: "extend/cowboy" },
       { :dynamo, "0.1.0-dev", github: "elixir-lang/dynamo" },
       { :json, github: "cblage/elixir-json"}
     ]
+  end
+
+  defp deps(:test) do
+    deps(:prod) ++
+      [ {:meck, "0.8.1", github: "eproxus/meck"} ]
+  end
+
+  defp deps(_) do
+    deps(:prod)
   end
 end
--- test/unit/github_test.exs
+++ test/unit/github_test.exs
@@ -6,4 +6,24 @@ defmodule GithubTest do
       "?client_id=the_github_client_id&scope=repo" <>
       "&redirect_uri=localhost%3A4000%2Fauthenticate"
   end
+
+  test "random_reviewer/3 does not return the current user" do
+    :meck.new(Github.Api)
+    :meck.expect(Github.Api, :contributors,
+      fn(_,_) ->
+        JSON.decode!(
+          "[{\"author\": {\"login\": \"duksis\"}, \"author\": {\"login\": \"paranoid\"}}]"
+        )
+      end
+    )
+    :meck.expect(Github.Api, :collaborators,
+      fn(_,_) -> JSON.decode! "[ {\"login\": \"duksis\"}, {\"login\": \"paranoid\"} ]" end
+    )
+
+    assert Github.random_reviewer(
+      "http://github.com/wimdu/who/pulls/1", "duksis", "valid token"
+    ) == ["paranoid"]
+
+    :meck.unload(Github.Api)
+  end
 end
